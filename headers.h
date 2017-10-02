7922833709c8b55cff3de8c710c5df3a936561e6
hliu3 <hliu3@stuy.edu>
2017-10-02 10:39:37 -0400

    headernemptyfile

---
+++ headers.h
@@ -0,0 +1,7 @@
+int strlen( char * );
+char * strncpy( char *dest, char *src, int n);
+char * strncat( char *dest, char *src, int n);
+int * strcmp( char *s1, char *s2);
+char * strchr( char *s, char c);
+char * strstr( char *s1, char *s2);
+
