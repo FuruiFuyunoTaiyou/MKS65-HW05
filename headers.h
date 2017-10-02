0fe7801f9ddac88c82e40ab6bd275a8852ddb7cf
hliu3 <hliu3@stuy.edu>
2017-10-02 10:57:43 -0400

    strlen

--- headers.h
+++ headers.h
@@ -1,7 +1,7 @@
-int strlen( char * );
-char * strncpy( char *dest, char *src, int n);
-char * strncat( char *dest, char *src, int n);
-int * strcmp( char *s1, char *s2);
-char * strchr( char *s, char c);
-char * strstr( char *s1, char *s2);
+int mstrlen( char * );
+char * mstrncpy( char *dest, char *src, int n);
+char * mstrncat( char *dest, char *src, int n);
+int * mstrcmp( char *s1, char *s2);
+char * mstrchr( char *s, char c);
+char * mstrstr( char *s1, char *s2);
 
--- strs.c
+++ strs.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include "headers.h"
 
+int mstrlen( char *s ) {
+  int i = 0;
+  while( s[i] ) {
+    i++;
+  }
+  return i;
+}
 
-int strlen
+int main() {
+  char *s0 = "";
+  char *s1 = "aa";
+  char *s2 = "aadjklsahflkjsdghlkadf";
+  printf("ours:\t%d\t%d\t%d\n",mstrlen(s0),mstrlen(s1),mstrlen(s2));
+  printf("sys:\t%d\t%d\t%d\n",strlen(s0),strlen(s1),strlen(s2));
+  return 0;
+}
